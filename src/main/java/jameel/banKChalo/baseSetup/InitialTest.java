53a50543762e72d5ad59526d05d83e9464947ea0
unknown <viraltomar@gmail.com>
2018-10-21 14:00:17 +0530

    jsonReader Added

--- src/main/java/jameel/banKChalo/baseSetup/InitialTest.java
+++ src/main/java/jameel/banKChalo/baseSetup/InitialTest.java
@@ -1,5 +1,6 @@
 package jameel.banKChalo.baseSetup;
 
+import java.io.FileNotFoundException;
 import java.lang.reflect.Method;
 
 import org.openqa.selenium.WebDriver;
@@ -13,11 +14,12 @@ import org.testng.annotations.Test;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
-
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
 import jameel.banKChalo.customListeners.CustomListeners;
 import jameel.banKChalo.testUtils.DriverManager;
 import jameel.banKChalo.testUtils.ExtentManager;
-import junit.framework.Assert;
+import jameel.banKChalo.testUtils.JSONReader;
 
 @Listeners(CustomListeners.class)
 public class InitialTest {
@@ -25,6 +27,7 @@ public class InitialTest {
 	public static ExtentReports extent;
 	public static ThreadLocal<ExtentTest> classLevelReport = new ThreadLocal<ExtentTest>();
 	public static ThreadLocal<ExtentTest> testLevelReport = new ThreadLocal<ExtentTest>();
+	JSONReader json = new JSONReader();
 	
 	@BeforeSuite
 	public void beforeSuite() {
@@ -61,18 +64,23 @@ public class InitialTest {
 	public void test2() {
 		driver.get("https://www.gmail.com");
 		testLevelReport.get().debug("Gmail Launched");
-		Assert.fail();
+		//Assert.fail();
 	}
 	
 	@Test(enabled=false)
 	public void test3() {
 		
 	}
-	
-	@AfterMethod
+		@AfterMethod
 	public void afterMethod() {
 		DriverManager.killDriverInstance();
 		extent.flush();
+	}
+	@org.testng.annotations.DataProvider
+	public Object[][] dataProvider(Method m) throws JsonIOException, JsonSyntaxException, FileNotFoundException{
+		return json.getData("C:\\\\Users\\\\VIRAL\\\\git\\\\SeleniumFramework\\\\resources\\\\test data\\\\Sample.json",m.getName().toString());
+		
+
 	}
 
 }
--- src/test/java/jameel/banKChalo/tests/MyFirstTest.java
+++ src/test/java/jameel/banKChalo/tests/MyFirstTest.java
@@ -1,5 +1,16 @@
 package jameel.banKChalo.tests;
 
+import java.util.HashMap;
+
+import org.testng.annotations.Test;
+
 public class MyFirstTest {
+	
+	@Test(dataProvider="dataProvider")
+	public void iPhone(HashMap<String,String> data)
+	{
+		System.out.println(data.get("TestCaseName"));
+		System.out.println(data.get("TestCaseID"));
+	}
 
 }
